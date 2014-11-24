1fa1ff155584b744f4a883541ce14c2ab0bd9845
Nicolas Flacco <nico@Nicolass-MacBook-Pro.local>
2014-11-24 12:38:35 -0800

    Initial Commit

---
+++ Location8Detector/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Location8Detector
+//
+//  Created by Nicolas Flacco on 11/24/14.
+//  Copyright (c) 2014 Nicolas Flacco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
