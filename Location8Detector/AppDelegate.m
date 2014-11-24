ccb9fa9ed4d9f50775d5130c069f1f67fc4734af
Nicolas Flacco <nico@Nicolass-MacBook-Pro.local>
2014-11-24 13:18:59 -0800

    should work

--- Location8Detector/AppDelegate.m
+++ Location8Detector/AppDelegate.m
@@ -7,16 +7,75 @@
 //
 
 #import "AppDelegate.h"
+@import CoreLocation;
 
-@interface AppDelegate ()
+// Define some constants
+#define BEACON_SERVICE_NAME     @"demo"
+#define BEACON_PROXIMITY_UUID   [[NSUUID alloc] initWithUUIDString:@"B9407F30-F5F8-466E-AFF9-25556B57F666"]
+
+@interface AppDelegate () <CLLocationManagerDelegate>
+
+@property (nonatomic, strong) CLLocationManager *locationManager;
+@property (nonatomic, strong) CLRegion *myRegion;
 
 @end
 
 @implementation AppDelegate
 
+// <Notification-based region monitoring>
+
+- (void)locationManager:(CLLocationManager *)manager
+didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    
+    // Check status to see if the app is authorized
+    BOOL canUseLocationNotifications = (status == kCLAuthorizationStatusAuthorizedWhenInUse);
+    
+    if (canUseLocationNotifications) {
+        [self startShowingLocationNotifications]; // Custom method defined below
+    }
+}
+
+- (void)startShowingLocationNotifications {
+    
+    UILocalNotification *locNotification = [[UILocalNotification alloc] init];
+    locNotification.alertBody = @"Your buddy is nearby";
+    locNotification.regionTriggersOnce = YES;
+    
+    CLBeaconRegion *beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:BEACON_PROXIMITY_UUID major:100 minor:400 identifier:BEACON_SERVICE_NAME];
+    
+    locNotification.region = beaconRegion;
+    
+    [[UIApplication sharedApplication] scheduleLocalNotification:locNotification];
+}
+
+- (void)application:(UIApplication *)application
+didReceiveLocalNotification: (UILocalNotification *)notification {
+    
+    CLRegion *region = notification.region;
+    
+    if (region) {
+        NSLog(@"XYZ");
+    }
+}
+
+// <\Notification-based region monitoring>
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    CLAuthorizationStatus status = [CLLocationManager authorizationStatus];
+    if (status != kCLAuthorizationStatusAuthorizedAlways) {
+        if ([self.locationManager respondsToSelector:@selector(requestAlwaysAuthorization)]) {
+            [self.locationManager requestAlwaysAuthorization];
+        } else {
+            // NOTE: Handle iOS7 deprecated stuff - NSLocationUsageDescription (see 2014 WWDC video)
+        }
+    }
+    self.locationManager.pausesLocationUpdatesAutomatically = YES;
+    // NOTE: We can always push the user to the iOS settings
+    
     return YES;
 }
 
